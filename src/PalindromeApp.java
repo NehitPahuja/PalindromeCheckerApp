b13f0ab969affb8980d89a9a795bf1e5b955a871
NehitPahuja <nehitpahuja@gmail.com>
2026-03-02 09:06:24 +0530

    UC1: Add welcome message

---
+++ src/PalindromeApp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PalindromeApp {
+
+
+    public static void main(String[] args){
+
+        //USE CASE 1
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
+
