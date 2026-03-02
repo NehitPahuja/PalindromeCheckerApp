1ef9ce96276d574d0409ab41bb370999a18e7554
NehitPahuja <nehitpahuja@gmail.com>
2026-03-02 10:08:26 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -8,6 +8,31 @@ public class PalindromeApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully");
+
+        //USE CASE 2
+        Scanner scanner = new Scanner(System.in); //
+        System.out.println("Enter a sentence:");
+        String inputString = scanner.nextLine();
+        //System.out.println("Entered String: "+inputString);
+
+        if(inputString == null || inputString.trim().isEmpty()){
+            System.out.println("Empty String, Please enter a String");
+            return;
+        }
+
+
+        boolean Isplaindrome = true;
+        for(int i=0;i<inputString.length()/2;i++){
+            if(inputString.charAt(i) != inputString.charAt((inputString.length())-i-1)) {
+                Isplaindrome = false;
+            }
+        }
+        if(Isplaindrome){
+            System.out.println("Is it a palindrome: " + Isplaindrome);
+        }
+        else{
+            System.out.println("Is it a palindrome: " + Isplaindrome);
+
+        }
     }
 }
-
